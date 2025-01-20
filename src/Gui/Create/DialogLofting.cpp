3920ab837c52caa6bd92d81a27ed9182e29202ef
ppoupon <ppoupon@gmail.com>
2025-01-20 19:10:30 +0100

    Loftingé

--- src/Gui/Create/DialogLofting.cpp
+++ src/Gui/Create/DialogLofting.cpp
@@ -73,7 +73,7 @@ namespace M3d {
     MySlider * cSliderPosY  = nullptr;
     MySlider * cSliderPosZ  = nullptr;
 
-    
+  
 
   public:
     DialogLofting() {;}
@@ -137,288 +137,295 @@ namespace M3d {
       
    
       
-  cSliderInter =  new MySliderInt(lX+5, lY, lW, lH, "Interpolate", SliderCB, this, 0, 64 );
-  cSliderInter->value( 0 );
-  lY += lYStep;
+      cSliderInter =  new MySliderInt(lX+5, lY, lW, lH, "Interpolate", SliderCB, this, 0, 64 );
+      cSliderInter->value( 0 );
+      lY += lYStep;
     
  	 
-  cSliderSpin =  new MySliderFloat(lX+5, lY, lW, lH, "Spin each step", SliderCB, this, -120, 120 );
-  cSliderSpin->value( 0 );
-  lY += lYStep;
+      cSliderSpin =  new MySliderFloat(lX+5, lY, lW, lH, "Spin each step", SliderCB, this, -120, 120 );
+      cSliderSpin->value( 0 );
+      lY += lYStep;
       
-  cSliderGrow =  new MySliderFloat(lX+5, lY, lW, lH, "Grow at each step", SliderCB, this, -5, 5 );
-  cSliderGrow->value( 0 );
-  lY += lYStep;
-
-
-      
-  lY += lYStep;
-  {
-    Fl_Group* o = new Fl_Group(lX, lY, lW+20, lH*7, "Position:");
-    o->box(FL_ENGRAVED_FRAME);
-    lY += lYStep;
-        
-    o->align(Fl_Align(FL_ALIGN_TOP_LEFT));
-        
-    cSliderCenterPosX = new MySlider(lX+5, lY, lW, lH, "Center X", SliderCB, this, -100, 100 );
-    cSliderCenterPosX->value( 0 );
-    lY += lYStep;
-        
-    cSliderCenterPosY = new MySlider(lX+5, lY, lW, lH, "Center Y", SliderCB, this, -100, 100 );
-    cSliderCenterPosY->value( 0 );
-    lY += lYStep;
-        
-    cSliderCenterPosZ = new MySlider(lX+5, lY, lW, lH, "Center Z", SliderCB, this, -100, 100 );
-    cSliderCenterPosZ->value( 0 );
-    lY += lYStep;
-        
-    o->end();
-  } // Fl_Group* o
+      cSliderGrow =  new MySliderFloat(lX+5, lY, lW, lH, "Grow at each step", SliderCB, this, -5, 5 );
+      cSliderGrow->value( 0 );
+      lY += lYStep;
 
 
       
+      lY += lYStep;
+      {
+        Fl_Group* o = new Fl_Group(lX, lY, lW+20, lH*7, "Position:");
+        o->box(FL_ENGRAVED_FRAME);
+        lY += lYStep;
+        
+        o->align(Fl_Align(FL_ALIGN_TOP_LEFT));
+        
+        cSliderCenterPosX = new MySlider(lX+5, lY, lW, lH, "Center X", SliderCB, this, -100, 100 );
+        cSliderCenterPosX->value( 0 );
+        lY += lYStep;
+        
+        cSliderCenterPosY = new MySlider(lX+5, lY, lW, lH, "Center Y", SliderCB, this, -100, 100 );
+        cSliderCenterPosY->value( 0 );
+        lY += lYStep;
+        
+        cSliderCenterPosZ = new MySlider(lX+5, lY, lW, lH, "Center Z", SliderCB, this, -100, 100 );
+        cSliderCenterPosZ->value( 0 );
+        lY += lYStep;
+        
+        o->end();
+      } // Fl_Group* o
 
-  lY += lYStep;
+
+      
+
+      lY += lYStep;
 			
 
-  { Fl_Button* o = new Fl_Button(210, lY, 75, 25, "Cancel");
-    o->callback((Fl_Callback*)CancelCB, this );
-  } // Fl_Button* o
+      { Fl_Button* o = new Fl_Button(210, lY, 75, 25, "Cancel");
+        o->callback((Fl_Callback*)CancelCB, this );
+      } // Fl_Button* o
       { Fl_Button* o = new Fl_Button(125, lY, 75, 25, "OK");
 	o->callback((Fl_Callback*)OkCB, this );
       } // Fl_Button* o
 	 
-
       
-  cMyWindow->end();
+      cMyWindow->end();
 		
-  cMyWindow->show( 0, nullptr);
+      cMyWindow->show( 0, nullptr);
 	
-  std::cout << "*********************************** FIN DIALOGUE **************************" << std::endl;
-  maj();
-}
+      std::cout << "*********************************** FIN DIALOGUE **************************" << std::endl;
+      maj();
+    }
     
-protected:   
+  protected:   
 
     
-//********************************************************************************
-// Calling when command's interface change
+    //********************************************************************************
+    // Calling when command's interface change
     
-void maj()
-{     
-  std::cout << "DialogLofting::maj0 " <<std::endl;
+    void maj()
+    {     
+      std::cout << "DialogLofting::maj0 " <<std::endl;
          
    
-  if( cMyTypeInput == TypeOfInput::INPUT_ENTRY
-      && (TheInput.getNbCurrentPoints() <  2 || TheInput.getCurrentLine() == nullptr
-      || TheSelect.getSelectType() != PP3d::SelectType::Facet
-      || TheSelect.getNbSelected() < 1
-          || TheSelect.getFirst()->getType() != PP3d::ShapeType::Facet ))
-    {          
-      return;
-    }
+      if( cMyTypeInput == TypeOfInput::INPUT_ENTRY
+          && (TheInput.getNbCurrentPoints() <  2 || TheInput.getCurrentLine() == nullptr
+              || TheSelect.getSelectType() != PP3d::SelectType::Facet
+              || TheSelect.getNbSelected() < 1
+              || TheSelect.getFirst()->getType() != PP3d::ShapeType::Facet ))
+        {          
+          return;
+        }
       
-  PP3d::Modif::ParamLoftingFacPath lParam;
+      PP3d::Modif::ParamLoftingFacPath lParam;
 
-  lParam.cFlagJoin   = (cCheckJoin->value() != 0 );
-  lParam.cFlagClose  = (cCheckClose->value() != 0 );
-  lParam.cFlagAlign  = (cCheckAlign->value() != 0 );
+      lParam.cFlagJoin   = (cCheckJoin->value() != 0 );
+      lParam.cFlagClose  = (cCheckClose->value() != 0 );
+      lParam.cFlagAlign  = (cCheckAlign->value() != 0 );
 
-  lParam.cNbInterpol = cSliderInter->value();
-  lParam.cSpin        = cSliderSpin->value();
-  lParam.cGrow        = cSliderGrow->value();
+      lParam.cNbInterpol = cSliderInter->value();
+      lParam.cSpin        = cSliderSpin->value();
+      lParam.cGrow        = cSliderGrow->value();
 
-  lParam.cLocalisation = cLocalisation;
+      lParam.cLocalisation = cLocalisation;
 
-  std::cout << "========== join:" << lParam.cFlagJoin << " " << cCheckJoin->value() << std::endl;
+      std::cout << "========== join:" << lParam.cFlagJoin << " " << cCheckJoin->value() << std::endl;
       
-  PP3d::Point3d lCenter;
+      PP3d::Point3d lCenter;
 
       
-  PP3d::FacetPtrVect lNewFacets;
+      PP3d::FacetPtrVect lNewFacets;
 
   
-  PPAutoPtr<Facet> lPath  = TheInput.getCurrentLine()->duplicate();	 
-  ObjBSpline lObjBSpline( "BSplineTmpLofting", lPath, false );
+      PPAutoPtr<Facet> lPath  = TheInput.getCurrentLine()->duplicate();	 
+      ObjBSpline lObjBSpline( "BSplineTmpLofting", lPath, false );
 
-  // ObjectPolylines      * lPath = TheInput.getCurrentLine();
-  //========= Interpolation par une BSpline  =========
-  if( lParam.cNbInterpol > 0 && TheInput.getNbCurrentPoints() >= 2 )
-    {
-      lObjBSpline.makePtsFromPoles( lParam.cNbInterpol );
-      lPath = lObjBSpline.getSplinePts();
-    }
-  //========= End Interpolation =========
-
-
-  if( cMyTypeInput == TypeOfInput::INPUT_ENTRY )
-    {  
-      PP3d::Modif::LoftingFacFromPath( &TheBase, (PP3d::FacetPtr)TheSelect.getFirst(),
-                                       lPath, lNewFacets, lParam );      
-     
-     if(  lNewFacets.size() > 0 )
-       {
-         PP3d::PolyPtr lShape = TheBase.getNewPoly();          
-         lShape->addFacet(lNewFacets);
-         
-         PP3d::ObjectPoly* lObjPoly =  new PP3d::ObjectPoly(  "Lofting", lShape );
-         
-         std::cout << "====== swapCurrentCreation :" << lObjPoly << std::endl;
-         TheInput.swapCurrentCreation( lObjPoly );  
-       }     
-    }
-  else    
-    if( cMyTypeInput == TypeOfInput::INPUT_OBJECT )
-      {
-        //======== INPUT_OBJECT =========       
-        std::unique_ptr<PP3d::DataBase> luTmpBase( new PP3d::DataBase() );
-
-        // On duplique la selection dans la base temporaire
-        //       Utils::DuplicateObject( TheBase,  TheSelect.getSelectionVect(), *luTmpBase );
-        std::stringstream  lDupStr0;
-        Utils::SaveObjectsInStream( TheBase, false, TheSelect.getSelectionVect(), lDupStr0);
-
-
-
-        PP3d::VisitorMinMax lVisitMinMax;
-        TheSelect.execVisitorOnlyOnObjects ( lVisitMinMax );
-        Point3d  lPtOld  =  lVisitMinMax.getCenter();
-
-        // Get all the point of the Path
-        PP3d::SortEntityVisitorPoint lVisitPath;
-        lPath->execVisitor( lVisitPath );         
-        GLuint lNbPt =  (GLuint)lVisitPath.cVectPoints.size();
-
-        for( GLuint i = 0; i<lNbPt; i++)  // Begining at index 1
-          {            
-            std::stringstream lDupStr( lDupStr0.str() );
-            std::vector<PP3d::EntityPtr> lNewObjs;
-            Utils::WriteObjectFromStream( lDupStr, *luTmpBase, lNewObjs );                 
-            PP3d::VisitorGetPoints<PP3d::PointPtrSet> lVisitPt;
-            for( PP3d::EntityPtr lObjPtr : lNewObjs )
-              lObjPtr->execVisitor( lVisitPt );
-                
-          //====== Move All to the begining of path       
-            Point3d lMove = lPath->getPoint(i)->get() - lPtOld; 
-                  
-            for(  const PP3d::PointPtr lPoint : lVisitPt.getPoints() )
-              {
-                lPoint->get() += lMove ;  // move point
-              }
-          }
-        //======= end move
-
-        
-
-        TheCreat.setDatabaseTmp( luTmpBase ); 
-      }
-
-       //--------------------------------------------------
-
-  TheCreat.redrawAllCanvas3d(PP3d::Compute::FacetAll);    
-}
-//----------------------------------------
-
-static void CancelCB( Fl_Widget*, void* pUserData ) {
- 
-  DialogLofting* lDialog = reinterpret_cast<DialogLofting*>(pUserData);
-
-      
-  //=============================
-  if( lDialog->cMyTypeInput == TypeOfInput::INPUT_ENTRY )
-    {        
-      TheInput.cancelCurrentCreation();
-    }
-  else if( lDialog->cMyTypeInput == TypeOfInput::INPUT_OBJECT )
-    {
-      TheCreat.clearDatabaseTmp();
-    }
-      
-  //=============================
-   
-
-  TheCreat.redrawAllCanvas3d( PP3d::Compute::FacetAll );
-
-  Fl::delete_widget( lDialog->cMyWindow );
-  Diag.cMyWindow = nullptr;
-}
-//----------------------------------------
-static void SliderCB( Fl_Widget*, void*pUserData )
-{
-  std::cout << "DialogLofting::SliderCB " << pUserData << std::endl;
-
-  DialogLofting* lDialog = reinterpret_cast<DialogLofting*>(pUserData);
-  lDialog->maj();
-}
-//----------------------------------------
-static  void SizeCB( Fl_Widget*, void*pUserData )
-{
-  std::cout << "DialogLofting::SizeCB " << pUserData << std::endl;
-  DialogLofting* lDialog = reinterpret_cast<DialogLofting*>(pUserData);
-  lDialog->maj();
-}
-//----------------------------------------
-static void SizeSliderCB( Fl_Widget*, void*pUserData )
-{
-  DialogLofting* lDialog = reinterpret_cast<DialogLofting*>(pUserData);
-  lDialog->maj(); 
-}
-//----------------------------------------
-static void CheckCB( Fl_Widget*, void*pUserData )
-{
-  DialogLofting* lDialog = reinterpret_cast<DialogLofting*>(pUserData);
-  lDialog->maj(); 
-}
-//----------------------------------------
-static void OkCB( Fl_Widget*, void*pUserData )
-{
-  DialogLofting* lDialog = reinterpret_cast<DialogLofting*>(pUserData);
-  lDialog->maj();
-
-  //=============================
-  if(  lDialog->cMyTypeInput == TypeOfInput::INPUT_ENTRY )
-    {
-      PP3d::Object* lObj = TheInput.validCurrentCreation(TheBase);
-   
-      if( lObj != nullptr )
+      // ObjectPolylines      * lPath = TheInput.getCurrentLine();
+      //========= Interpolation par une BSpline  =========
+      if( lParam.cNbInterpol > 0 && TheInput.getNbCurrentPoints() >= 2 )
         {
-          lObj->rename(  "Lofting"  );
+          lObjBSpline.makePtsFromPoles( lParam.cNbInterpol );
+          lPath = lObjBSpline.getSplinePts();
         }
+      //========= End Interpolation =========
+
+
+      if( cMyTypeInput == TypeOfInput::INPUT_ENTRY )
+        {  
+          PP3d::Modif::LoftingFacFromPath( &TheBase, (PP3d::FacetPtr)TheSelect.getFirst(),
+                                           lPath, lNewFacets, lParam );      
+     
+          if(  lNewFacets.size() > 0 )
+            {
+              PP3d::PolyPtr lShape = TheBase.getNewPoly();          
+              lShape->addFacet(lNewFacets);
+         
+              PP3d::ObjectPoly* lObjPoly =  new PP3d::ObjectPoly(  "Lofting", lShape );
+         
+              std::cout << "====== swapCurrentCreation :" << lObjPoly << std::endl;
+              TheInput.swapCurrentCreation( lObjPoly );  
+            }   
+        }
+      else    
+        if( cMyTypeInput == TypeOfInput::INPUT_OBJECT )
+          {
+            //======== INPUT_OBJECT =========       
+            std::unique_ptr<PP3d::DataBase> luTmpBase( new PP3d::DataBase() );
+
+            // On duplique la selection dans la base temporaire
+            //       Utils::DuplicateObject( TheBase,  TheSelect.getSelectionVect(), *luTmpBase );
+            std::stringstream  lDupStr0;
+            Utils::SaveObjectsInStream( TheBase, false, TheSelect.getSelectionVect(), lDupStr0);
+
+
+
+            PP3d::VisitorMinMax lVisitMinMax;
+            TheSelect.execVisitorOnlyOnObjects ( lVisitMinMax );
+            Point3d  lCenter  =  lVisitMinMax.getCenter();
+
+            // Get all the point of the Path
+            PP3d::SortEntityVisitorPoint lVisitPath;
+            
+            lPath->execVisitor( lVisitPath );         
+            GLuint lNbPt =  (GLuint)lVisitPath.cVectPoints.size();
+            
+            PP3d::Point3d lNormToMove( lPath->getPoint(0)->get());
+            lNormToMove.normalize();
+            
+            for( GLuint i = 0; i<lNbPt; i++)  // Begining at index 1
+              {            
+                std::stringstream lDupStr( lDupStr0.str() );
+                std::vector<PP3d::EntityPtr> lNewObjs;
+                Utils::WriteObjectFromStream( lDupStr, *luTmpBase, lNewObjs );                 
+                PP3d::VisitorGetPoints<PP3d::PointPtrSet> lVisitPt;
+                for( PP3d::EntityPtr lObjPtr : lNewObjs )
+                  lObjPtr->execVisitor( lVisitPt );
+                
+                //====== Move All to the begining of path       
+                Point3d lMove = lPath->getPoint(i)->get() - lCenter;
+            
+                if( lParam.cFlagAlign )
+                  {                                      
+                    PP3d::Point3d lMoveNorm( lMove );
+                    lMoveNorm.normalize();
+              
+                    PP3d::Mat4 lMatAlign;
+                    lMatAlign.rotateAlign( lMoveNorm, lNormToMove  ); // for align the two vector  
+          
+                    for( PP3d::PointPtr lPoint : lVisitPt.getPoints()  )
+                      { 
+                        lPoint->get() -= lCenter;    // put at 0,0,0 
+                        lPoint->get() *= lMatAlign;      // rotation for alignment
+                        lPoint->get() += lCenter;    // move to it's orignal position
+                        lPoint->get() += lMove;
+                      }
+                  }
+                else                
+                  for(  const PP3d::PointPtr lPoint : lVisitPt.getPoints() )
+                    {
+                      lPoint->get() += lMove ;  // move point                  
+                    }  
+              }            
+            TheCreat.setDatabaseTmp( luTmpBase ); 
+          }
+      //--------------------------------------------------
+
+      TheCreat.redrawAllCanvas3d(PP3d::Compute::FacetAll);    
     }
-  else
-    if(  lDialog->cMyTypeInput == TypeOfInput::INPUT_OBJECT )
-      {
-        TheCreat.validateDatabaseTmp();
-      }
-  //=============================
-
-          
-  PushHistory();
-      
-
-  TheCreat.redrawAll(PP3d::Compute::FacetAll);
-
-          
-  Fl::delete_widget(lDialog->cMyWindow);  // Normly if I am understand the documentation, it will destroy all the children
-  Diag.cMyWindow = nullptr;
-}
-//************************
-
-
+    //----------------------------------------
     
-};
+    static void CancelCB( Fl_Widget*, void* pUserData )
+    {      
+      DialogLofting* lDialog = reinterpret_cast<DialogLofting*>(pUserData);      
+      
+      //=============================
+      if( lDialog->cMyTypeInput == TypeOfInput::INPUT_ENTRY )
+        {        
+          TheInput.cancelCurrentCreation();
+        }
+      else if( lDialog->cMyTypeInput == TypeOfInput::INPUT_OBJECT )
+        {
+          TheCreat.clearDatabaseTmp();
+        }
+      
+      //=============================
+   
+      
+      TheCreat.redrawAllCanvas3d( PP3d::Compute::FacetAll );
+
+      Fl::delete_widget( lDialog->cMyWindow );
+      Diag.cMyWindow = nullptr;
+    }
+    //----------------------------------------
+    static void SliderCB( Fl_Widget*, void*pUserData )
+    {
+      std::cout << "DialogLofting::SliderCB " << pUserData << std::endl;
+
+      DialogLofting* lDialog = reinterpret_cast<DialogLofting*>(pUserData);
+      lDialog->maj();
+    }
+    //----------------------------------------
+    static  void SizeCB( Fl_Widget*, void*pUserData )
+    {
+      std::cout << "DialogLofting::SizeCB " << pUserData << std::endl;
+      DialogLofting* lDialog = reinterpret_cast<DialogLofting*>(pUserData);
+      lDialog->maj();
+    }
+    //----------------------------------------
+    static void SizeSliderCB( Fl_Widget*, void*pUserData )
+    {
+      DialogLofting* lDialog = reinterpret_cast<DialogLofting*>(pUserData);
+      lDialog->maj(); 
+    }
+    //----------------------------------------
+    static void CheckCB( Fl_Widget*, void*pUserData )
+    {
+      DialogLofting* lDialog = reinterpret_cast<DialogLofting*>(pUserData);
+      lDialog->maj(); 
+    }
+    //----------------------------------------
+    static void OkCB( Fl_Widget*, void*pUserData )
+    {
+      DialogLofting* lDialog = reinterpret_cast<DialogLofting*>(pUserData);
+      lDialog->maj();
+
+      //=============================
+      if(  lDialog->cMyTypeInput == TypeOfInput::INPUT_ENTRY )
+        {
+          PP3d::Object* lObj = TheInput.validCurrentCreation(TheBase);
+   
+          if( lObj != nullptr )
+            {
+              lObj->rename(  "Lofting"  );
+            }
+        }
+      else
+        if(  lDialog->cMyTypeInput == TypeOfInput::INPUT_OBJECT )
+          {
+            TheCreat.validateDatabaseTmp();
+          }
+      //=============================
+          
+      PushHistory();
+      
+      TheCreat.redrawAll(PP3d::Compute::FacetAll);
+             
+      Fl::delete_widget(lDialog->cMyWindow);  // Normly if I am understand the documentation, it will destroy all the children
+      Diag.cMyWindow = nullptr;
+    }
+    //************************
+  };
 } // namespace M3d
 
-//************************
+    
+  //************************
 extern void CallDialogLofting( TypeOfInput iTypeInput )
 {
   cout << "CallDialogLofting " <<  endl;
-  
+      
   if( Diag.isAlreadyRunning() == false )
     {
       Diag.init( iTypeInput  );     
     }
 }
 //************************
-
-
